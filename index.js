6fc97c47527bbba802bbdb9516b4b0bfd3d89c53
pedro <pramos.meta@alelo.com.br>
2022-09-21 15:33:14 -0300

    General updates

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const puppeteer = require('puppeteer-extra')
 // Enable stealth plugin with all evasions
 puppeteer.use(require('puppeteer-extra-plugin-stealth')());
 
+const chromeLauncher = require('chrome-launcher');
+
+const chromeConfig = {
+        chromePath: "/usr/bin/google-chrome-stable"
+}
+
 //Create Sleep function to use in Async/Await function
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -11,60 +17,86 @@ const randomDelay = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
 
 (async () => {
-
   await sleep(1000);
 
-  // Launch the browser in headless mode and set up a page.
   const browser = await puppeteer.launch({
     slowMo: 250, // slow down by 250ms
-    dumpio: true,
     headless: false,
-    channel: 'chrome',
-    // executablePath: '/usr/bin/google-chrome-stable',
+    // channel: 'chrome',
+     executablePath: chromeConfig.chromePath,
     ignoreHTTPSErrors: true,
 
-    args: ['--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-web-security',
-            '--disable-infobars',
-            '--enable-automation',
-            '--window-position=0,0',
-            '--ignore-certifcate-errors',
-            '--ignore-certifcate-errors-spki-list',
-            // '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'
-    ],
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-web-security',
+      '--disable-infobars',
+      '--enable-automation',
+      '--window-position=0,0',
+      '--ignore-certifcate-errors',
+      '--ignore-certifcate-errors-spki-list',
 
+      // '--disable-accelerated-2d-canvas',
+      '--no-zygote',
+      '--renderer-process-limit=1',
+      '--no-first-run',
+      '--disable-dev-shm-usage',
+      '--disable-infobars',
+      '--lang=en-US,en',
+      '--window-size=1920x1080',
+      '--disable-extensions',
+      // '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"'
+    ],
   })
+
   const page = await browser.newPage()
 
-  await page.setUserAgent(
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
-  );
-  await page.setExtraHTTPHeaders({
-    'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
-  });
+  await page.emulateTimezone("Asia/Singapore");
+    // await page.setUserAgent(
+  //   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+  // );
 
-  await page.setViewport({ width: 800, height: 600 })
+  // await page.setViewport({width: (width/2)-21, height: height-111});
+
+  // await page.setExtraHTTPHeaders({
+  //   'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+  // });
+
+  await page.setViewport({
+    width: 800, height: 600
+  })
+
   await page.goto('https://www.bet365.com')
 
-  await page.$eval(".hm-MainHeaderRHSLoggedOutMed_Login", elem => elem.click());
+  // await page.$eval(".hm-MainHeaderRHSLoggedOutMed_Login", elem => elem.click());
+
+  await page.click(".hm-MainHeaderRHSLoggedOutMed_Login",
+    {
+      timeout: 1000, delay: randomDelay(3000, 6000)
+    })
 
   const selectorUsername = '.lms-StandardLogin_Username';
-  await page.waitForSelector(selectorUsername, { visible: true, timeout: 3000, delay: randomDelay(300, 600) });
+  await page.waitForSelector(selectorUsername,
+    {
+      visible: true, timeout: 3000, delay: randomDelay(2000, 5000)
+    });
 
   const selectorPassword = '.lms-StandardLogin_Password';
-  await page.waitForSelector(selectorPassword, { visible: true, timeout: 3000, delay: randomDelay(300, 600) });
-
-  await page.type(selectorUsername, "user",
+  await page.waitForSelector(selectorPassword,
     {
-      delay: randomDelay(200, 300)
-    });
-  await page.type(selectorPassword, "123",
-    {
-      delay: randomDelay(200, 300)
+      visible: true, timeout: 3000, delay: randomDelay(3000, 6000)
     });
 
-  await page.keyboard.press('Enter', { delay: randomDelay(300, 600) });
+  await page.type(selectorUsername, "USUÁRIO",
+    {
+      delay: randomDelay(2000, 6000)
+    });
+  await page.type(selectorPassword, "SENHA",
+    {
+      delay: randomDelay(2000, 3000)
+    });
+
+  await page.keyboard.press('Enter', { delay: randomDelay(3000, 6000) });
 
   // await browser.close()
 })()
